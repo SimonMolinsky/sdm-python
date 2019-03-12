ddc6fecaecb627c2d5889c966541b6eda56bae82
szymon-datalions <s.molinski@datalions.eu>
2019-03-12 17:59:18 +0100

    initial structure

---
+++ project/a_data_preparation/get_climate_datasets.py
@@ -0,0 +1,7 @@
+"""Climate Datasets Downloader
+
+This script allows the user to download climate datasets from the Coperniucs Climate Change Serivice
+by cdsapi package. Script automatizes download and it prepares logs from the whole process.
+
+...
+"""

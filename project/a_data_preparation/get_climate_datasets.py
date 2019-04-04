c0fe102bf7360e8dbdbb3501c7a1a7426ad4430b
szymon-datalions <s.molinski@datalions.eu>
2019-04-04 09:55:24 +0200

    corrections in docstrings for get_climate_data

--- project/a_data_preparation/get_climate_datasets.py
+++ project/a_data_preparation/get_climate_datasets.py
@@ -31,11 +31,12 @@ class DataRequest:
         self.datasets_description = available_datasets[1]
         self.output = output_folder
 
-
     def prepare_requests(self, input_information=None):
         """
         Function prepares input for a request to retrieve Copernicus ERA5 single level
         reanalysis datasets in the netcdf format.
+
+        INPUT:
         :param input_information: input_data about spatial and temporal scale of a dataset in the form of Python list:
         [variable number (to check numbers invoke DataRequest object with argument data_dict or print
         DataRequest object),
@@ -44,6 +45,8 @@ class DataRequest:
         list of days from 1 to 31 as a strings,
         list of hours from 00:00 to 23:00 as a strings,
         output filename without '.nc' ending]
+
+        OUTPUT:
         :return: cds api request
         """
         if self.interactive:
@@ -72,7 +75,6 @@ class DataRequest:
         )
         return request_text
 
-
     def _initialize_datasets(self):
         """If you want to add new datasets do it inside this method.
         To make everything readable and clear create new variable for each parameter and update data type dictionary
@@ -80,7 +82,11 @@ class DataRequest:
         Example: 2m Temperature
         temperature2m = ['2m_temperature', '2m Temperature;]
         Example: Some Parameter
-        some_parameter = ['variable name for cds api request', 'variable name']"""
+        some_parameter = ['variable name for cds api request', 'variable name']
+
+        OUTPUT:
+        :return dictionary with datasets, description text for an interactive request setting
+        """
 
         # VARIABLES
         temperature2m = ['2m_temperature', '2m Temperature']  # Create variables based on this example below
@@ -100,9 +106,21 @@ class DataRequest:
 
         return data_type_dict, description_text
 
-
     def _get_input_data(self):
+        """If interactive session is selected then this method serves as the communication channel between an user
+        and a program. All text is printed in terminal and at the same place user provides information about needed
+        variables.
 
+        OUTPUT:
+        :return input_info: A list in the form: [
+                                                    selected dataset,
+                                                    [list of years to get the data],
+                                                    [list of months to get the data],
+                                                    [list of days to get the data],
+                                                    [list of hours to get the data],
+                                                    output filename
+                                                ]
+        """
         input_info = []
 
         # Select variable
@@ -111,7 +129,7 @@ class DataRequest:
 
         # Select years
         year_selection_start = 'Please provide start and end year of the analysis:\n' \
-                         'Start year (number in format XXXX, min. 1979, max. 2018, press RETURN):\n'
+                               'Start year (number in format XXXX, min. 1979, max. 2018, press RETURN):\n'
         year_selection_end = 'End year (number in format XXXX, min. 1979, max. 2018, press RETURN):\n'
         y_start = int(input(year_selection_start))
         y_end = int(input(year_selection_end))
@@ -119,7 +137,7 @@ class DataRequest:
 
         # Select month
         month_selection_start = 'Please provide start and end month of the analysis:\n' \
-                               'Start month (number from 1 to 12, press RETURN):\n'
+                                'Start month (number from 1 to 12, press RETURN):\n'
         month_selection_end = 'End month (number from 1 to 12, press RETURN):\n'
         m_start = int(input(month_selection_start))
         m_end = int(input(month_selection_end))
@@ -131,9 +149,9 @@ class DataRequest:
         input_info.append(days)
 
         # Select hours
-        select_hours = '0: 00:00, \n1: 06:00, \n2: 12:00, \n3: 18:00, \n4: 00:00 + 06:00 + 12:00 + 18:00, \n5: all hours' \
-                       'from 00:00 to 23:00. \nType number and press RETURN\n'
-        hours = int(input(select_hours))
+        slt_hours = '0: 00:00, \n1: 06:00, \n2: 12:00, \n3: 18:00, \n4: 00:00 + 06:00 + 12:00 + 18:00, \n5: all hours' \
+                    'from 00:00 to 23:00. \nType number and press RETURN\n'
+        hours = int(input(slt_hours))
         hours_dict = {0: '00:00',
                       1: '06:00',
                       2: '12:00',
@@ -152,7 +170,6 @@ class DataRequest:
         input_info.append(output_filename + name + '.nc')
         return input_info
 
-
     def __str__(self):
         output = ''
         for key in self.data_dict:
@@ -161,9 +178,10 @@ class DataRequest:
 
 
 def download_climate_data(request_text):
+    """Method uses cdsapi Client to get the requested data"""
     try:
         c = cdsapi.Client()
-    except Exception:
+    except Exception:  # too broad exception, narrow it
         print(Exception)
         print('Follow tutorial here: https://cds.climate.copernicus.eu/api-how-to to configure an api')
         return 0
